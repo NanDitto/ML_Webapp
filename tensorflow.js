b756e57ca51224f86ab5b4dfc8648c9a8fc649d8
Nandor Gallo <nandor.gallo@uoit.net>
2021-04-13 13:17:03 -0400

    Tensorflow.js Function works with tensor flow

--- tensorflow.js
+++ tensorflow.js
@@ -1,20 +1,18 @@
-//import * as tf from '@tensorflow/tfjs-node';
+
 
 const tf = require('@tensorflow/tfjs');
-require('@tensorflow/tfjs-node');
 
-console.log(model);
-
-async function prediction(image1, image2){
+//WORKING
+function prediction(){
   const model = await tf.loadLayersModel('model/model.json');
-
-  //Make a prediction using the model
+  // Convert Image to tensorflow Image
   const person1_image = tf.fromPixels(image1);
   const person2_image = tf.fromPixels(image2);
 
+  //Make a prediction using the model
   const prediction1 = model.predict(person1_image);
   const prediction2 = model.predict(person2_image);
 
-
-  //return 'rock' , 'paper' -> Person1, Person2
+  //Returns an array of indexes [n, n]
+  return [prediction1.indexOf(Math.max(...prediction1)), prediction2.indexOf(Math.max(...prediction2))]
 }
